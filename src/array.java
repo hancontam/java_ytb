9ebd7ba3615b720bdd00191ea5a619727faf8e2b
tamcon <hancontam@gmail.com>
2024-09-22 12:12:59 +0700

    fix dat ten

--- src/array.java
+++ src/array.java
@@ -1,9 +1,12 @@
-public class main {
-    public static void main(String[] args) throws Exception {
+public class array {
+
+    public static void main(String[] args) {
+
         // on ham array
         // cach xuat array phai tostring
         // import lib/cac func hay cua array
         // thuat toan sap xep array
-        // 3 bai tap
+        // 3 bai tap}
+        System.out.println("hj");
     }
 }
